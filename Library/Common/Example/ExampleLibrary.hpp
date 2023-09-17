0df7d10a37493b7b1f83c14e9f85d7c6f0be1288
arminpcm <mostafa.parchami@gmail.com>
2023-09-17 12:54:27 -0400

    Added readme

--- Library/Common/Example/ExampleLibrary.hpp
+++ Library/Common/Example/ExampleLibrary.hpp
@@ -1,3 +1,17 @@
+// Copyright 2023 Armin Parchami. All rights reserved.
+
+// This software is provided "as is," without warranty of any kind, express or implied,
+// including but not limited to the warranties of merchantability,
+// fitness for a particular purpose and noninfringement.
+// In no event shall the authors or copyright holders be liable for any claim,
+// damages or other liability, whether in an action of contract,
+// tort or otherwise, arising from, out of or in connection with
+// the software or the use or other dealings in the software.
+
+/// @brief A function to add two integer numbers
+/// @param a The first integer number
+/// @param b The second integer number
+/// @return sum of the two input arguments
 int add(int a, int b) {
     return a + b;
 }
