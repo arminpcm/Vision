70e5de35900587e5504b2d6219bd54a09d33970d
arminpcm <mostafa.parchami@gmail.com>
2023-09-17 20:03:56 -0400

    Implemented a baseline

--- Library/ConfigInterface/ConfigInterface.hpp
+++ Library/ConfigInterface/ConfigInterface.hpp
@@ -8,29 +8,64 @@
 // tort or otherwise, arising from, out of or in connection with
 // the software or the use or other dealings in the software.
 
+#include <stdexcept>
 #include <iostream>
 #include <string>
 #include <yaml-cpp/yaml.h>
 
 namespace vision {
 namespace config {
+
+/// @brief Config interface class that allows users to get serialized representation of config object
+/// @tparam T The struct used to define config
+template <typename T>
 class ConfigInterface {
 public:
+    /// @brief deleted default constructor
     ConfigInterface() = delete;
-    explicit ConfigInterface(const std::string& file_name);
-    virtual ~ConfigInterface();
 
-    virtual bool LoadConfig();
+    /// @brief Constructor loading config given a filename
+    /// @param file_name The path to the YAML file
+    explicit ConfigInterface(const std::string& file_name) noexcept : file_name_(file_name) {
+        LoadConfig();
+    };
 
+    /// @brief Default destructor defined as virtual to allow inheritence
+    virtual ~ConfigInterface() = default;
 
+    /// @brief Loads config
+    virtual void LoadConfig() {
+        try {
+            config_ = YAML::LoadFile(file_name_);
+        } catch (const YAML::Exception& e) {
+            std::cerr << "Error parsing YAML: " << e.what() << std::endl;
+        }
+    };
+
+    /// @brief Serializes the config
+    /// @return Returns serialized representation of the config
+    /// @throw Throws std::runtime if the config_ object is empty
+    T GetConfigObject() {
+        if (config_.IsNull()) {
+            throw std::runtime_error("Cannot serialize an empty config");
+        }
+        return config_object_.Serialize(config_);
+    }
+
+    // Deleted move and copy semantics 
     ConfigInterface(ConfigInterface& other) = delete;
     ConfigInterface(ConfigInterface&& other) = delete;
     ConfigInterface operator=(ConfigInterface& other) = delete;
     ConfigInterface operator=(ConfigInterface&& other) = delete;
+
 protected:
-    YAML::Node getConfig() const;
+    /// @brief Protected method defined to provide access to config_ object for children
+    /// @return YAML::Node representing the config
+    YAML::Node GetConfig() const { return config_; };
+
 private:
     YAML::Node config_;
+    T config_object_;
     std::string file_name_;
 };
 }  // namespace config
