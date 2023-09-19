f60cc8ad4ec74942a532a6e3e19706d3fdcb8ea6
Armin PCM <10624445+arminpcm@users.noreply.github.com>
2023-09-18 23:21:15 -0400

    Armin dev 9 add component (#14)

---
+++ Application/Example/ExampleArgumentsParser.cc
@@ -0,0 +1,42 @@
+// Copyright 2023 Armin Parchami. All rights reserved.
+
+// This software is provided "as is," without warranty of any kind, express or implied,
+// including but not limited to the warranties of merchantability,
+// fitness for a particular purpose and noninfringement.
+// In no event shall the authors or copyright holders be liable for any claim,
+// damages or other liability, whether in an action of contract,
+// tort or otherwise, arising from, out of or in connection with
+// the software or the use or other dealings in the software.
+
+#include <iostream>
+#include <gflags/gflags.h>
+
+DEFINE_string(topic_name, "", "Name of the topic");  // NOLINT
+DEFINE_int32(queue_size, 0, "Size of the queue");    // NOLINT
+
+
+int main(int argc, char* argv[]) {
+    // Initialize gflags
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    // Check if the required flags are provided
+    if (FLAGS_topic_name.empty()) {
+        std::cerr << "Error: topic_name flag is required." << std::endl;
+        return 1;
+    }
+
+    if (FLAGS_queue_size <= 0) {
+        std::cerr << "Error: queue_size must be a positive integer." << std::endl;
+        return 1;
+    }
+
+    // Access the parsed values
+    std::string topic_name = FLAGS_topic_name;
+    int queue_size = FLAGS_queue_size;
+
+    // Use the parsed values in your application
+    std::cout << "Topic Name: " << topic_name << std::endl;
+    std::cout << "Queue Size: " << queue_size << std::endl;
+
+    return 0;
+}
--- WORKSPACE
+++ WORKSPACE
@@ -70,3 +70,10 @@ http_archive(
 )
 load("@com_google_protobuf//:protobuf_deps.bzl", "protobuf_deps")
 protobuf_deps()
+
+http_archive(
+    name = "gflags",
+    sha256 = "34af2f15cf7367513b352bdcd2493ab14ce43692d2dcd9dfc499492966c64dcf",
+    urls = ["https://github.com/gflags/gflags/archive/v2.2.2.tar.gz"],
+    strip_prefix = "gflags-2.2.2",
+)
